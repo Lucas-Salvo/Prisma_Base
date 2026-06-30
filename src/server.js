146d19c4939850a4b7631a55e766a9c847af5f71
LUCAS CORREIA SALVO <lucas_salvo@estudante.sesisenai.org.br>
2026-06-30 08:16:25 -0300

    Primeiro commit

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
-import express from 'express';
-import { Prisma } from '@prisma/client/extension';
+import express from "express";
+import { prisma } from "./lib/prisma.ts";
+import cors from "cors"
+
 const app = express()
 const PORT = 3000
 
+app.use (cors())
 app.use(express.json())
+
+app.get("/produtos", async (req, res) => {
+    try{
+    const itens = await prisma.produtos.findMany()
+    res.json(itens)
+    }catch(error){
+        res.status(500).json({error: "Erro ao retornar produtos"})
+    }
+})
+
+app.post("/produtos", async (req, res) =>{
+       const {nome, categoria, quantidade} = req.body
+    try{
+        const novoItem = await prisma.produtos.create({
+            data: {nome, categoria, quantidade: Number (quantidade)}
+        })
+        res.status(201).json(novoItem)
+    }catch(error){
+        res.status(400).json({error: "Erro ao criar um novo item"})
+    }
+})
 
 app.listen(PORT, () => {
     console.log("Server Rodando na porta " + PORT)
